7612b243600911a98a200365008e144e6a227c1d
Mike Renfro <renfro@tntech.edu>
2024-06-21 09:10:47 -0500

    add extra echos

--- pi/test_pi_runs.sh
+++ pi/test_pi_runs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo '"Good" runs:'
+echo "- OpenMP"
 for cpt in 1 2 4; do
     srun --nodes=1 --ntasks-per-node=1 --cpus-per-task=${cpt} --threads-per-core=1 ./pi_omp
 done
 
+echo "- MPI"
 npn_cpt_list="1,1 1,2 1,4 2,1 2,2 4,1"
 for nodes in 1 2; do
     for npn_cpt in npn_cpt_list; do
@@ -14,6 +16,7 @@ for nodes in 1 2; do
 done
 
 echo '"Bad" runs:'
+echo "- OpenMP"
 for cpt in 1 2 4; do
     srun --nodes=1 --ntasks-per-node=1 --cpus-per-task=${cpt} --threads-per-core=2 ./pi_omp
 done
