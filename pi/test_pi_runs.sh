c56725fef656d50d4081582149bd2ff383fd7452
Mike Renfro <renfro@tntech.edu>
2024-06-21 09:19:57 -0500

    correct variable notation

--- pi/test_pi_runs.sh
+++ pi/test_pi_runs.sh
@@ -6,12 +6,12 @@ for cpt in 1 2 4; do
 done
 
 echo "- MPI"
-npn_cpt_list="1,1 1,2 1,4 2,1 2,2 4,1"
+tpn_cpt_list="1,1 1,2 1,4 2,1 2,2 4,1"
 for nodes in 1 2; do
-    for npn_cpt in ${npn_cpt_list}; do
-        npn=$(echo ${npn_cpt} | cut -d, -f1)
-        cpt=$(echo ${npn_cpt} | cut -d, -f2)
-        srun --nodes=${nodes} --ntasks-per-node=${npn} --cpus-per-task=${cpt} --threads-per-core=1 ./pi_omp_mpi
+    for tpn_cpt in ${tpn_cpt_list}; do
+        tpn=$(echo ${tpn_cpt} | cut -d, -f1)
+        cpt=$(echo ${tpn_cpt} | cut -d, -f2)
+        srun --nodes=${nodes} --ntasks-per-node=${tpn} --cpus-per-task=${cpt} --threads-per-core=1 ./pi_omp_mpi
     done
 done
 
